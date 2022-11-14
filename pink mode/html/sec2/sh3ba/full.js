2925f19c49d2416c1da75e113efe0b0b59d7c64f
abobak2007 <abazacons@gmail.com>
2022-11-14 08:32:03 +0200

    first commit

---
+++ pink mode/html/sec2/sh3ba/full.js
@@ -0,0 +1,51 @@
+let $input = $("input[type=\"text\"][id^=\"fullName\"]");
+
+$(document).ready(function() {
+    generatePlaceholders();
+    makeNamePartListItemsDraggable($("#ulFirstNames"), $("#ulLastNames"));
+    makeFullNameTextBoxesAsDropTargets();
+});
+
+function generatePlaceholders() {
+    $input.each((index, element) => {
+        let id = element.getAttribute("id").toString();
+        let number = id.substring("fullName".length);
+        let n = parseInt(number);
+
+    });
+}
+
+function makeNamePartListItemsDraggable(...lists) {
+    $.each(lists, function(index, element) {
+        $list = $(element);
+        let li = $($list).children("li");
+        li.attr("draggable", "true");
+        li.on("dragstart", namePartListItemDragStartEventHandler);
+    });
+}
+
+function namePartListItemDragStartEventHandler(event) {
+    let dataTransfer = event.dataTransfer || event.originalEvent.dataTransfer;
+    dataTransfer.setData("text", $(this).text());
+}
+
+function makeFullNameTextBoxesAsDropTargets() {
+    $input.on("drop", function(event) {
+        let e = event.originalEvent || event;
+
+        let namePart = e.dataTransfer.getData("text");
+        let existingText = $(this).val().trim();
+
+        if (existingText.split(" ").length > 1) {
+            return false;
+        }
+
+        if (existingText === "") {
+            $(this).val(namePart);
+        } else {
+            $(this).val(existingText.trimEnd() + " " + namePart);
+        }
+
+        e.preventDefault();
+    });
+}
